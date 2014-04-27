42928a93bfd250a8d36112cfdf3f55fb9bcd0d2c
eulerphi <dekken@joshuas-air.soulfoodcoffeehouse.com>
2014-04-27 11:13:08 -0700

    Problem 8

--- 8.hs
+++ 8.hs
@@ -1,2 +1,19 @@
-compress :: (Eq a) => [a] -> [a]
+import Data.List as List
 
+-- Original solution
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:y:xs) | x == y = compress (x:xs)
+compress (x:y:xs) | x /= y = x : compress (y:xs)
+
+-- Cleaned up version of above
+compress' :: (Eq a) => [a] -> [a]
+compress' (x:xs@(y:_))
+	| x == y = compress' xs
+	| otherwise = x : compress' xs
+compress' x = x
+
+-- Functional version...
+compress'' :: Eq a => [a] -> [a]
+compress'' = map head . List.group
